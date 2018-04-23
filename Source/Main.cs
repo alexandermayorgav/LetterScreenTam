ea52345e4ad1347cbafc81cf239089942dddf740
Alexander Mayorga <alexandermayorga@outlook.com>
2018-04-23 13:19:16 -0500

    cambios

--- Source/Main.cs
+++ Source/Main.cs
@@ -9,15 +9,13 @@ public class App
 	{	
 		try
 		{
-            //Jpt.Start("jpt.ini");      // do this once on app start
-            //     	Jpt.Use();                 // do this for each document
+           // Jpt.Start("jpt.ini");      // do this once on app start
+            //Jpt.Use();                 // do this for each document
             //Jpt.End();                 // do this once on app end
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmEstatus());
+
         }
 
-		catch(Exception ex)
+        catch (Exception ex)
 		{
 			MessageBox.Show(ex.Message, "JPT Error",MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
 		}
